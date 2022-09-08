50763698b4a402825ee4152c7b1fc102680775ae
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-08 10:43:24 +0300

    Not final version of Problem F of OZON Route 256 sandbox

--- ozon/route256/sandbox/problem_f/problem_f.go
+++ ozon/route256/sandbox/problem_f/problem_f.go
@@ -6,6 +6,11 @@ import (
 	"os"
 )
 
+// https://leetcode.com/discuss/interview-question/1408251/4-single-source-shortest-path-in-undirected-graph
+// https://www.geeksforgeeks.org/shortest-path-unweighted-graph/
+// https://stackoverflow.com/questions/2818852/is-there-a-queue-implementation
+// https://stackoverflow.com/questions/3601180/calculate-distance-between-2-nodes-in-a-graph
+
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
@@ -14,9 +19,25 @@ func main() {
 		_ = out.Flush()
 	}(out)
 
-	var setCount int
-	_, _ = fmt.Fscan(in, &setCount)
+	var n int
+	var m int
+	_, _ = fmt.Fscan(in, &n, &m)
 
-	for i := 0; i < setCount; i++ {
+	adj := make([][]int, n)
+
+	for i := 0; i < m; i++ {
+		var u int
+		var v int
+
+		_, _ = fmt.Fscan(in, &u, &v)
+
+		addEdge(adj, u-1, v-1)
 	}
+
+	_, _ = fmt.Fprintln(out, "!")
+}
+
+func addEdge(adj [][]int, u int, v int) {
+	adj[u] = append(adj[u], v)
+	adj[v] = append(adj[v], u)
 }
--- ozon/route256/sandbox/problem_f/problem_f_test.go
+++ ozon/route256/sandbox/problem_f/problem_f_test.go
@@ -19,3 +19,164 @@ func Test02(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func Test03(t *testing.T) {
+	err := utils.RunTest(main, "tests\\03", "tests\\03.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test04(t *testing.T) {
+	err := utils.RunTest(main, "tests\\04", "tests\\04.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test05(t *testing.T) {
+	err := utils.RunTest(main, "tests\\05", "tests\\05.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test06(t *testing.T) {
+	err := utils.RunTest(main, "tests\\06", "tests\\06.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test07(t *testing.T) {
+	err := utils.RunTest(main, "tests\\07", "tests\\07.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test08(t *testing.T) {
+	err := utils.RunTest(main, "tests\\08", "tests\\08.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test09(t *testing.T) {
+	err := utils.RunTest(main, "tests\\09", "tests\\09.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test10(t *testing.T) {
+	err := utils.RunTest(main, "tests\\10", "tests\\10.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test11(t *testing.T) {
+	err := utils.RunTest(main, "tests\\11", "tests\\11.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test12(t *testing.T) {
+	err := utils.RunTest(main, "tests\\12", "tests\\12.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test13(t *testing.T) {
+	err := utils.RunTest(main, "tests\\13", "tests\\13.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test14(t *testing.T) {
+	err := utils.RunTest(main, "tests\\14", "tests\\14.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test15(t *testing.T) {
+	err := utils.RunTest(main, "tests\\15", "tests\\15.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test16(t *testing.T) {
+	err := utils.RunTest(main, "tests\\16", "tests\\16.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test17(t *testing.T) {
+	err := utils.RunTest(main, "tests\\17", "tests\\17.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test18(t *testing.T) {
+	err := utils.RunTest(main, "tests\\18", "tests\\18.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test19(t *testing.T) {
+	err := utils.RunTest(main, "tests\\19", "tests\\19.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test20(t *testing.T) {
+	err := utils.RunTest(main, "tests\\20", "tests\\20.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test21(t *testing.T) {
+	err := utils.RunTest(main, "tests\\01", "tests\\01.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test22(t *testing.T) {
+	err := utils.RunTest(main, "tests\\02", "tests\\02.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test23(t *testing.T) {
+	err := utils.RunTest(main, "tests\\03", "tests\\03.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test24(t *testing.T) {
+	err := utils.RunTest(main, "tests\\04", "tests\\04.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test25(t *testing.T) {
+	err := utils.RunTest(main, "tests\\05", "tests\\05.a")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
