f90d7045cd4c19c4f655c014114a4228646bd226
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-08 10:43:35 +0300

    Not final version of Problem G of OZON Route 256 sandbox

--- ozon/route256/sandbox/problem_g/problem_g.go
+++ ozon/route256/sandbox/problem_g/problem_g.go
@@ -6,6 +6,9 @@ import (
 	"os"
 )
 
+// https://www.geeksforgeeks.org/number-of-connected-components-of-a-graph-using-disjoint-set-union/
+// https://dou.ua/lenta/articles/union-find/
+
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
