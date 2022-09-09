dc3b15d32867b2bd5b5ba7588ee5b5c3ba65a6af
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-09 03:57:55 +0300

    Final version of Problem F of OZON Route 256 sandbox

--- ozon/route256/sandbox/problem_f/problem_f.go
+++ ozon/route256/sandbox/problem_f/problem_f.go
@@ -4,12 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
-
-// https://leetcode.com/discuss/interview-question/1408251/4-single-source-shortest-path-in-undirected-graph
-// https://www.geeksforgeeks.org/shortest-path-unweighted-graph/
-// https://stackoverflow.com/questions/2818852/is-there-a-queue-implementation
-// https://stackoverflow.com/questions/3601180/calculate-distance-between-2-nodes-in-a-graph
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
@@ -21,6 +17,7 @@ func main() {
 
 	var n int
 	var m int
+
 	_, _ = fmt.Fscan(in, &n, &m)
 
 	adj := make([][]int, n)
@@ -34,10 +31,70 @@ func main() {
 		addEdge(adj, u-1, v-1)
 	}
 
-	_, _ = fmt.Fprintln(out, "!")
+	for i := 0; i < n; i++ {
+		sf := bfs(adj, i)
+		for j := 0; j < len(sf); j++ {
+			_, _ = fmt.Fprint(out, sf[j])
+			if j < len(sf)-1 {
+				_, _ = fmt.Fprint(out, " ")
+			}
+		}
+		_, _ = fmt.Fprintln(out)
+	}
 }
 
 func addEdge(adj [][]int, u int, v int) {
 	adj[u] = append(adj[u], v)
 	adj[v] = append(adj[v], u)
+}
+
+func bfs(adj [][]int, s int) []int {
+	result := make([]int, 0)
+
+	v2 := make(map[int]int)
+
+	lengths := make(map[int]int)
+	visited := make(map[int]bool)
+
+	q := make([]int, 0)
+
+	visited[s] = true
+	q = append(q, s)
+
+	for len(q) != 0 {
+		u := q[0]
+		q = q[1:]
+
+		for _, v := range adj[u] {
+			if !visited[v] {
+				lengths[v] = lengths[u] + 1
+				if lengths[v] == 2 {
+					v2[v]++
+				} else {
+					visited[v] = true
+					q = append(q, v)
+				}
+			}
+		}
+	}
+
+	if len(v2) == 0 {
+		return []int{0}
+	} else {
+		var max = -1
+		for p := range v2 {
+			if max < v2[p] {
+				max = v2[p]
+			}
+		}
+		for p := range v2 {
+			if v2[p] == max {
+				result = append(result, p+1)
+			}
+		}
+	}
+
+	sort.Ints(result)
+
+	return result
 }
